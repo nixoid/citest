2e1156cfcea9d4a9bc925114a6cc78ae91a935d5
Andrey Zamovskiy <nihuhoid@gmail.com>
2014-11-18 13:53:01 -0800

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Citest::Application.config.secret_key_base = 'c83d8f77f195d9714cf429807f04aaf6cc43a5a04932b2925a135d18af7b9cef1c9b3cf70bbcd7307b5d835b3d4e5929cb3737e3cbef989aef528cd63d4b0767'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Citest::Application.config.session_store :cookie_store, key: '_citest_session'
